05034284dc678f43c791244dda1ee44d8be6950f
gene <geneathome@livemail.tw>
2020-04-08 17:16:06 +0800

    j

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -8,7 +8,7 @@ DigitalOut redLED(LED1);
 DigitalOut greenLED(LED2);
 BusOut display(D6, D7, D9, D10, D11, D5, D4, D8);
 
-char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+char seven[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 float period = 0;
 int flag = 0;
 int sample = 0;
@@ -28,24 +28,18 @@ int main(){
   while(1){
     if( Switch == 0){
       greenLED = 1;
-      redLED = 0;
-        
-      display = table[freq/100];
+      redLED = 0;        
+      display = seven[freq/100];
       wait(0.5);        
-
-      display = table[freq/10%10];
+      display = seven[freq/10%10];
       wait(0.5);
-
-      if( i%10== 0)
-          display = 0xBF;
-      else
-          display = table[freq%10];
+      display = 0xBF;
       wait(0.5);
     }
     else{
       greenLED = 0;
       redLED = 1;
-      display = 00000000;
+      display = 0;
       if(freq != 0){
         for(j = 0; j < 2; j += 0.05){
           Aout = 0.5 + 0.5 * sin(j*3.14159);
